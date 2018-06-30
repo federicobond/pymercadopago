3f47c2453a1c04311e45edc49d715dea63f90db9
Federico Bond <federicobond@gmail.com>
2018-06-30 07:43:24 -0300

    Relax requests requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        "requests>=2.19.1",
+        "requests>=2.1.0",
         "future",
     ],
     setup_requires=[
