73c09a6c54327b79b43bf38fd921ca30e43ef77b
Federico Bond <federicobond@gmail.com>
2018-06-30 07:27:09 -0300

    Add keywords to setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
-    ]
-
+    ],
+    keywords='mercadopago api'
 )
