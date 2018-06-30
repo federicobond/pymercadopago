14be48dedc06e97ca65431d04fb1f2872067793e
Federico Bond <federicobond@gmail.com>
2018-06-30 07:44:15 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="pymercadopago",
-    version="0.1.0",
+    version="0.1.1",
     description="An API wrapper for MercadoPago",
     long_description=read('README.rst'),
     author="Federico Bond",
