f332a0df3fe8a76932016a99c41104fb1830cb44
Federico Bond <federicobond@gmail.com>
2018-06-30 03:53:23 +0200

    Export errors from main module

--- mercadopago/__init__.py
+++ mercadopago/__init__.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from .api import Client
+from .errors import (
+    Error,
+    AuthenticationError,
+    NotFoundError,
+    BadRequestError
+)
 
-__all__ = ['Client']
+__all__ = [
+    'Client',
+    'Errors',
+    'AuthenticationError',
+    'NotFoundError',
+    'BadRequestError'
+]
+
